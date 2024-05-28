12305b97bae3f424076938e4eab977ffdd0cca83
Grace Debeila <dgrace74@gmail.com>
2024-05-28 21:25:59 +0200

    Update scripts and add progress bar implementation

---
+++ .ipynb_checkpoints/Collect_API_Data-checkpoint.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+def get_from_oecd(sdmx_query):
+    return pd.read_csv(
+        f"https://stats.oecd.org/SDMX-JSON/data/{sdmx_query}?contentType=csv",
+        dtype={'Reference Period Code': str, 'Reference Period': str},
+        low_memory=False
+    )
+
+sdmx_query_CPI = "PRICES_CPI/all"
+df_CPI = get_from_oecd(sdmx_query_CPI)
+
+sdmx_query_LTRATES = "MEI_FIN/all"
+df_LTRATES = get_from_oecd(sdmx_query_LTRATES)
+
+df_CPI_filtered = df_CPI[(df_CPI['MEASURE'] == 'IXOB') & ((df_CPI['FREQUENCY'] == 'M') | (df_CPI['FREQUENCY'] == 'Q')) & (df_CPI['SUBJECT'] == 'CPALTT01')]
+df_CPI_filtered.to_csv('OECD_CPI_Data.csv', index=False)
+print("Saved OECD CPI Data")
+
+df_LTRATES_filtered = df_LTRATES[(df_LTRATES['FREQUENCY'] == 'M') & (df_LTRATES['SUBJECT'] == 'IRLT')]
+df_LTRATES_filtered.to_csv('OECD_LTRATES_Data.csv', index=False)
+print("Saved OECD LTRATES Data")
+
+
